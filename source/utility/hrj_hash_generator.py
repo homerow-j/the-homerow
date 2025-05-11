510b27de106e87d0392a27dc84e769fcf8389aa5
homerow-j <jake_nix@pm.me>
2025-05-11 13:43:00 -0700

    it now returnes the result and did some renaming

--- source/utility/hrj_hash_generator.py
+++ source/utility/hrj_hash_generator.py
@@ -4,7 +4,7 @@ import random
 import secrets
 
 
-def passwd_gen():
+def hrj_hash_generator():
     initial_generation_hex = secrets.token_hex(32768)
     initial_generation_url_safe = secrets.token_hex(32768)
     initial_bucket = initial_generation_hex + initial_generation_url_safe
@@ -13,9 +13,10 @@ def passwd_gen():
     shaken_bucket = random.sample(l_bucket, k=65536)
     n_char = int((input
                   ("How many characters do you want in the generated hash?: ")))
-    passwd_pick = random.sample(shaken_bucket, k=n_char)
-    passwd = "".join(passwd_pick)
-    print(passwd)
+    hash_pick = random.sample(shaken_bucket, k=n_char)
+    generated_hash = "".join(hash_pick)
+    print(generated_hash)
+    return generated_hash
 
 
-passwd_gen()
+hrj_hash_generator()
