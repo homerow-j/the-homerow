fe247e59fe893262cb9478d4c10042a459cddf7a
homerow-j <jake_nix@pm.me>
2025-05-20 13:19:52 -0700

    optimization to hrj_hash_generator

--- source/utility/hrj_hash_generator.py
+++ source/utility/hrj_hash_generator.py
@@ -7,32 +7,32 @@ import secrets
 def hhg():
     random_bucket_01 = secrets.token_urlsafe(4096)
     random_bucket_02 = secrets.token_urlsafe(4096)
-    cumulative_bucket_01 = random_bucket_01 + random_bucket_02
+    cumulative_bucket = random_bucket_01 + random_bucket_02
     del random_bucket_01, random_bucket_02
     random_bucket_03 = secrets.token_urlsafe(4096)
-    cumulative_bucket_02 = cumulative_bucket_01 + random_bucket_03
-    del random_bucket_03, cumulative_bucket_01
+    cumulative_bucket = cumulative_bucket + random_bucket_03
+    del random_bucket_03
     random_bucket_04 = secrets.token_urlsafe(4096)
-    cumulative_bucket_03 = cumulative_bucket_02 + random_bucket_04
-    del random_bucket_04, cumulative_bucket_02
+    cumulative_bucket = cumulative_bucket + random_bucket_04
+    del random_bucket_04
     random_bucket_05 = secrets.token_urlsafe(4096)
-    cumulative_bucket_04 = cumulative_bucket_03 + random_bucket_05
-    del cumulative_bucket_03, random_bucket_05
+    cumulative_bucket = cumulative_bucket + random_bucket_05
+    del random_bucket_05
     random_bucket_06 = secrets.token_urlsafe(4096)
-    cumulative_bucket_05 = cumulative_bucket_04 + random_bucket_06
-    del cumulative_bucket_04, random_bucket_06
+    cumulative_bucket = cumulative_bucket + random_bucket_06
+    del random_bucket_06
     random_bucket_07 = secrets.token_urlsafe(4096)
-    cumulative_bucket_06 = cumulative_bucket_05 + random_bucket_07
-    del cumulative_bucket_05, random_bucket_07
+    cumulative_bucket = cumulative_bucket + random_bucket_07
+    del random_bucket_07
     random_bucket_08 = secrets.token_urlsafe(4096)
-    cumulative_bucket_07 = cumulative_bucket_06 + random_bucket_08
-    del cumulative_bucket_06, random_bucket_08
+    cumulative_bucket_07 = cumulative_bucket + random_bucket_08
+    del random_bucket_08
     random_bucket_09 = secrets.token_urlsafe(4096)
-    cumulative_bucket_08 = cumulative_bucket_07 + random_bucket_09
-    del cumulative_bucket_07, random_bucket_09
+    cumulative_bucket = cumulative_bucket + random_bucket_09
+    del random_bucket_09
     random_bucket_10 = secrets.token_urlsafe(4096)
-    filled_bucket = cumulative_bucket_08 + random_bucket_10
-    del cumulative_bucket_08, random_bucket_10
+    filled_bucket = cumulative_bucket + random_bucket_10
+    del cumulative_bucket, random_bucket_10
     n_char = int((input("# of characters (40960 max.): ")))
     hash_pick = random.sample(filled_bucket, k=n_char)
     hhg_result = "".join(hash_pick)
